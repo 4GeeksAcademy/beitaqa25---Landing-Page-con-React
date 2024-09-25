ea5a7673fb4d52bbe6eccbde5ba36949c43adb52
beitaqa25 <mari01beita@gmail.com>
2024-09-25 14:59:45 -0600

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,51 @@
 import React from "react";
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
+import Card from "./card";
 
-//include images into your bundle
+// Include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+// Create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    return (
+        <div>
+            <Navbar />
+            <div className="container mt-5 ">
+                <Jumbotron />
+                <div className="row">
+                    <div className="col-md-3">
+                        <Card
+                            title="Card title 1"
+                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos natus non laboriosam!"
+                            buttonText="Find Out More!"
+                        />
+                    </div>
+                    <div className="col-md-3">
+                        <Card
+                            title="Card title 2"
+                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos natus non laboriosam!"
+                            buttonText="Find Out More!"
+                        />
+                    </div>
+                    <div className="col-md-3">
+                        <Card
+                            title="Card title 3"
+                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos natus non laboriosam!"
+                            buttonText="Find Out More!"
+                        />
+                    </div>
+                    <div className="col-md-3">
+                        <Card
+                            title="Card title 4"
+                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos natus non laboriosam!"
+                            buttonText="Find Out More!"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
